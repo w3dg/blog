cebd9c3a273da26a51a7ebae7172b6d012b3d036
w3dg <51162936+w3dg@users.noreply.github.com>
2024-02-08 03:03:39 +0530

    Update social buttons component to have icons

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,39 +1,47 @@
-import { LuArrowRight } from "react-icons/lu";
+import { LuInstagram, LuGithub } from "react-icons/lu";
+import { FaXTwitter } from "react-icons/fa6";
+import { IconType } from "react-icons";
 import { dmsans } from "./fonts";
 import Link from "next/link";
 
 interface SocialLink {
   name: string;
   link: string;
+  icon: IconType;
 }
 
-const SocialButton = ({ link }: { link: SocialLink }) => {
+interface SocialButtonProps {
+  link: SocialLink;
+  icon: IconType;
+}
+
+const SocialButton = ({ link, icon: Icon }: SocialButtonProps) => {
   return (
     <Link
       href={link.link}
       target="_blank"
       className="flex items-center justify-center gap-2 p-3 rounded-md hover:bg-brand-700 bg-brand-800 group"
     >
-      {link.name}
-      <span className="p-0.5 transition-colors -rotate-45 rounded-full group-hover:bg-brand-800">
-        <LuArrowRight></LuArrowRight>
+      <span className="p-1 transition-colors rounded-full group-hover:bg-brand-800">
+        <Icon></Icon>
       </span>
+      {link.name}
     </Link>
   );
 };
 
 const Socials = () => {
   const socialLinks: SocialLink[] = [
-    { name: "instagram", link: "https://instagram.com/its_not_dg" },
-    { name: "twitter", link: "https://twitter.com/its_not_dg" },
-    { name: "github", link: "https://github.com/w3dg" },
+    { name: "instagram", link: "https://instagram.com/its_not_dg", icon: LuInstagram },
+    { name: "twitter", link: "https://twitter.com/its_not_dg", icon: FaXTwitter },
+    { name: "github", link: "https://github.com/w3dg", icon: LuGithub },
   ];
   return (
     <section className={`${dmsans.className} flex flex-col items-center mt-4`}>
       <p>Follow on socials: </p>
       <div className="flex gap-2 mt-2">
         {socialLinks.map((link) => {
-          return <SocialButton key={link.name} link={link} />;
+          return <SocialButton key={link.name} link={link} icon={link.icon} />;
         })}
       </div>
     </section>
